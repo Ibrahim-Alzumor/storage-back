34efd91d5e89b5300b63590dfccb25dd29ab9563
i.alzumor <i.alzumor@bisan.com>
2025-05-15 13:48:53 +0300

    added validators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,30 @@
+import {
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+  @IsEmail()
   email: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
+  @IsString()
+  @IsOptional()
   jobTitle?: string;
+  @IsString()
+  @IsOptional()
   phone?: string;
+  @IsNumber()
+  @IsNotEmpty()
   clearanceLevel: number;
 }
